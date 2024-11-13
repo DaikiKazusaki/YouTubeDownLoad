7058800efd227f6da0fafb0c86bf86d450af2476
Daiki Kazusaki <130907832+DaikiKazusaki@users.noreply.github.com>
2024-11-13 14:09:06 +0900

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -4,4 +4,4 @@ from yt_dlp import YoutubeDL
 
 #動画のURLを指定
 yt_opts = YoutubeDL({'format':'best'})
-yt_opts.download(['https://www.youtube.com/watch?v=iFyKRRA9TY8&t=320s'])
+yt_opts.download(['URL'])
